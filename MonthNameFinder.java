a364f945d0a87c2fac37c2958a06f25c5aa4826e
Rupesh7077 <rt6370897@gmail.com>
2026-08-26 23:31:43 +0530

    Added MonthNameFinder file successfully

--- MonthNameFinder.java
+++ MonthNameFinder.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class MonthNameFinder {
+public class MonthNameFinder{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
         System.out.println("Enter number of month:");
